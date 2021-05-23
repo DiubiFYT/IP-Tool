5cd6ad94199cbb37041a01e9f46ebf1d6743c3bc
albi <albi.rava.003@gmail.com>
2021-05-23 19:26:49 +0200

    bho

--- IP Tool/JS/Main.js
+++ IP Tool/JS/Main.js
@@ -198,7 +198,7 @@ function OctectToBinary(octect){
 function IPToBinary(str){
 
     if(!IsAValidDottedDecimal(str)){
-        console.log(str + " it's not a valid IP!");
+        console.log(str + " is not a valid IP!");
         return null;
     }
 
@@ -489,24 +489,16 @@ function GetUsedHosts(IP, subnets, nSubnets){
 
     for(let i = 0; i < subnetsNHost.length; i++){
 
-        let gatewayIp = subnets[i].gatewayIp;
+        let binaryIP = IPToBinary(IP);
+        let lastRange = binaryIP[3];
+        let lastSum = "";
 
-        let gatewayIpSplitted = gatewayIp.split(".");
-        let firstRange = "";
-        for(let j = 0; j < 4; j++){
-            if(j != index){
-                firstRange += parseInt(gatewayIpSplitted[j]) + ".";
-            }else{
-                firstRange += parseInt(gatewayIpSplitted[j]) + 1;
-            }
-        }
+        if(lastSum != "11111111"){
+            lastSum = addBinary(lastSum, 1);
+            console.log(lastSum);
 
-        let secondRange = "";
-        for(let j = 0; j < 4; j++){
-            if(j != index){
-                secondRange += parseInt(gatewayIpSplitted[j]) + ".";
-            }else{
-                secondRange += parseInt(gatewayIpSplitted[j]) + parseInt(subnetsNHost[i]);
+            if(i == subnetsNHost.length - 1){
+                lastRange = last
             }
         }
 
@@ -538,3 +530,42 @@ function GetSubnetsHosts(){
     })
     return data;
 }
+
+function halfAdder(a, b){
+    const sum = xor(a,b);
+    const carry = and(a,b);
+    return [sum, carry];
+  }  
+function fullAdder(a, b, carry){
+    halfAdd = halfAdder(a,b);
+    const sum = xor(carry, halfAdd[0]);
+    carry = and(carry, halfAdd[0]);
+    carry = or(carry, halfAdd[1]);
+    return [sum, carry];
+}  
+
+  function xor(a, b){return (a === b ? 0 : 1);}
+  function and(a, b){return a == 1 && b == 1 ? 1 : 0;}
+  function or(a, b){return (a || b);}
+  
+function addBinary(a, b){
+  
+    let sum = '';
+    let carry = '';
+  
+    for(var i = a.length-1;i>=0; i--){
+      if(i == a.length-1){
+        //half add the first pair
+        const halfAdd1 = halfAdder(a[i],b[i]);
+        sum = halfAdd1[0]+sum;
+        carry = halfAdd1[1];
+      }else{
+        //full add the rest
+        const fullAdd = fullAdder(a[i],b[i],carry);
+        sum = fullAdd[0]+sum;
+        carry = fullAdd[1];
+      }
+    }
+  
+    return carry ? carry + sum : sum;
+  }
